56ee80f5e09b5cd6746f745316ac8c575d333cad
Peishu <oceanoia@gmail.com>
2024-04-03 21:43:52 +0800

    新增 商品詳細頁面+樣式

--- frontend-react/pages/shop/index.js
+++ frontend-react/pages/shop/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import ShopProduct from '@/components/shop/shop-product'
+import ShopProduct from '@/components/product/shop-product'
 import Head from 'next/head'
+// import Loader from '@/components/product-b/loader'
+// import LoadingBar from 'react-top-loading-bar'
 
 export default function Shop() {
   return (
