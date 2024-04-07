e1e6dad6e5e93b58be2e2b19ab141789168b8920
Peishu <oceanoia@gmail.com>
2024-04-07 22:53:37 +0800

    完成訂單頁+樣式

--- frontend-react/components/cart/order-confirmation.js
+++ frontend-react/components/cart/order-confirmation.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Link from 'next/link'
 import style from '@/styles/order-confirmation-main.module.scss'
 import { IMG_PATH } from '@/configs'
 
@@ -8,30 +9,52 @@ export default function OrderConfirmationMain() {
       <section className={`${style['order-confirmation']} ${style['spad']}`}>
         <div className={`container pt-3`}>
           {/* 感謝收到訂單 */}
-          <div className={style['order-confirmation-content']}>
-            <div className={`row justify-content-center`}>
+          <div className={`d-flw ${style['order-confirmation-content']}`}>
+            <div
+              className={`row justify-content-center ${style['order-confirmation-img']}`}
+            >
               <img
-                src={`${IMG_PATH}/materials/thankyou.png`}
+                src={`${IMG_PATH}/materials/delivery.gif`}
                 alt=""
-                style={{ height: 700, width: 700 }}
+                style={{
+                  height: 350,
+                  width: 500,
+                  marginTop: '45px',
+                  marginBottom: '60px',
+                }}
               />
             </div>
             <div className={style['order-confirmation-message']}>
               <div className={`mb-2 ${style['order-number']}`}>
-                <h5 className={`row justify-content-center`}>
+                <h6 className={`row justify-content-center`}>
                   訂單編號:
-                  <span className={`row justify-content-center`}>
-                    #35789621
-                  </span>
-                </h5>
+                  <Link
+                    href="#"
+                    className={`row justify-content-center`}
+                    style={{ color: '#EB6234' }}
+                  >
+                    <span className={`row justify-content-center`}>
+                      #35789621
+                    </span>
+                  </Link>
+                </h6>
               </div>
-              <h2 className={`row justify-content-center mb-3`}>
+              <h2
+                className={`row justify-content-center mt-4 mb-4`}
+                style={{ fontWeight: '700' }}
+              >
                 感謝您的購買!
               </h2>
-              <h4 className={`row justify-content-center mb-2`}>
+              <h4
+                className={`row justify-content-center mb-2`}
+                style={{ fontSize: '18px' }}
+              >
                 您將會在短期內收到一封訂單確認通知郵件。
               </h4>
-              <h4 className={`row justify-content-center mb-4`}>
+              <h4
+                className={`row justify-content-center mb-4`}
+                style={{ fontSize: '18px' }}
+              >
                 請點選以下查詢訂單，確認商品備貨狀態。
               </h4>
             </div>
@@ -55,7 +78,7 @@ export default function OrderConfirmationMain() {
           {/* 您可能也會喜歡 */}
           <div className={style['recommend-products']}>
             <div className={style['recommend-products-title']}>
-              <h3>您可能也會喜歡...</h3>
+              <h3>...</h3>
             </div>
           </div>
         </div>
