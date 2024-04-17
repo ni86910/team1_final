20bacbc692d89ae609c10f0c034ee655742dc434
jack <qwqw979796@gmail.com>
2024-04-17 22:49:42 +0800

    更新 常見問題樣式 1

--- frontend-react/components/accordion/Myaccordion.js
+++ frontend-react/components/accordion/Myaccordion.js
@@ -1,31 +1,29 @@
 import React, { useState } from 'react'
-import style from '@/styles/jack-use/accordion.module.css'
-import { FaAngleUp, FaAngleDown } from 'react-icons/fa6'
+import Accordion from 'react-bootstrap/Accordion'
 
 export default function MyAccordion({ question, answer }) {
-  const [show, setShow] = useState(false)
-
-  // 處理點擊事件的函數
-  const handleHeaderClick = () => {
-    setShow(!show)
-  }
-
   return (
-    <div className={style['accordion-container']}>
-      <div
-        //  role="presentation" 在div 設定onClick需要加這段
-        role="presentation"
-        className={`${style['accordion-header']} ${
-          show ? style['active'] : ''
-        }`}
-        onClick={handleHeaderClick}
-      >
-        {question}
-        <div className={style['accordion-btn']}>
-          {show ? <FaAngleUp /> : <FaAngleDown />}
-        </div>
-      </div>
-      {show && <p className={style['answers']}>{answer}</p>}
-    </div>
+    // <div className={style['accordion-container']}>
+    //   <div
+    //     //  role="presentation" 在div 設定onClick需要加這段
+    //     role="presentation"
+    //     className={`${style['accordion-header']} ${
+    //       show ? style['active'] : ''
+    //     }`}
+    //     onClick={handleHeaderClick}
+    //   >
+    //     {question}
+    //     <div className={style['accordion-btn']}>
+    //       {show ? <FaAngleUp /> : <FaAngleDown />}
+    //     </div>
+    //   </div>
+    //   {show && <p className={style['answers']}>{answer}</p>}
+    // </div>
+    <Accordion defaultActiveKey={[]} alwaysOpen>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>{question}</Accordion.Header>
+        <Accordion.Body>{answer}</Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   )
 }
