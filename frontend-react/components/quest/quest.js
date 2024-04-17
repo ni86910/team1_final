f4924bdbe2b2c2f1d97ddcbda356046ad2c2427f
jack <qwqw979796@gmail.com>
2024-04-17 22:55:53 +0800

    更新 常見問題樣式 2

--- frontend-react/components/quest/quest.js
+++ frontend-react/components/quest/quest.js
@@ -16,20 +16,15 @@ export default function Quest() {
 
   return (
     <>
-      <Container>
+      <Container style={{ marginBottom: 20 }}>
         <Row>
           <h3 className="mt-4 text-center">常見問題</h3>
-          <h5 className="mt-4 text-center" style={{ marginBottom: 20 }}>
+          <h5 className="mt-4 text-center" style={{ marginBottom: 15 }}>
             請透過下方分類快速找尋您的疑問，若仍無法獲得解答，您可以透過聯繫我們單元，直接與我們聯繫！
           </h5>
         </Row>
 
-        <Tabs
-          defaultActiveKey="profile"
-          id="uncontrolled-tab-example"
-          className="mb-3"
-          justify
-        >
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" justify>
           <Tab eventKey="home" title="場館相關">
             {data.map((curElem) => {
               const { id } = curElem
