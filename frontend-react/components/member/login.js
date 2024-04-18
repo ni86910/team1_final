e592f7d0a8760fde96cffb985ac723771e5c40ee
chichi <lative911@gmail.com>
2024-04-18 22:50:06 +0800

    功能完成: 顯示密碼,記住帳密

--- frontend-react/components/member/login.js
+++ frontend-react/components/member/login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useAuth } from '@/context/auth-context'
 import { useRouter } from 'next/router'
 import Swal from 'sweetalert2'
@@ -8,10 +8,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import { FaStarOfLife } from 'react-icons/fa6'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 export default function LoginPage() {
   const router = useRouter()
   const { login } = useAuth()
+
+  // 呈現密碼用
+  const [inputText, setInputText] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
+  // 驗證
   const [data, setData] = useState({
     m_account: '',
     m_pwd: '',
@@ -21,6 +28,23 @@ export default function LoginPage() {
     m_pwd: '',
   })
 
+  // 新增Remember Me 狀態
+  const [rememberMe, setrememberMe] = useState(false)
+
+  useEffect(() => {
+    const rememberAccount = localStorage.getItem('rememberAccount')
+    const rememberPwd = localStorage.getItem('rememberPwd')
+    const rememberState = localStorage.getItem('rememberState')
+
+    if (rememberState === 'true' && rememberAccount && rememberPwd) {
+      setData({ m_account: rememberAccount, m_pwd: rememberPwd })
+      setrememberMe(true)
+    }
+  }, [])
+
+
+
+  // 驗證表單字段
   const validateFields = () => {
     const newErrors = {}
 
@@ -34,11 +58,11 @@ export default function LoginPage() {
       newErrors.m_pwd = '密碼為必填欄位'
     } else if (
       !validator.isStrongPassword(data.m_pwd, {
-        minLength: 5,
-        minLowercase: 1,
-        minUppercase: 0,
-        minNumbers: 0,
-        minSymbols: 0,
+        minLength: 5,         // 最小字元數
+        minLowercase: 1,      // 最少要幾個小寫英文字元
+        minUppercase: 0,      // 最少要幾個大寫英文字元
+        minNumbers: 0,        // 最少要幾個數字
+        minSymbols: 0,        // 最少要幾個符號
       })
     ) {
       newErrors.m_pwd = '密碼至少5個字元，要包含一個英文小寫字元'
@@ -48,26 +72,38 @@ export default function LoginPage() {
     return Object.values(newErrors).every((error) => error === '')
   }
 
+  // 提交表單
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     if (!validateFields()) {
-      return
+      return;
     }
 
-    const { m_account, m_pwd } = data
+    const { m_account, m_pwd } = data;
 
     try {
-      const result = await login(m_account, m_pwd)
+      const result = await login(m_account, m_pwd);
       if (result) {
+        console.log(result)
+        if(rememberMe){
+          localStorage.setItem('rememberAccount', m_account);
+          localStorage.setItem('rememberPwd', m_pwd);
+          localStorage.setItem('rememberState', true);
+        }else{
+          localStorage.removeItem('rememberAccount');
+          localStorage.removeItem('rememberPwd');
+          localStorage.removeItem('rememberState');
+        }
+
         Swal.fire({
           position: 'top',
           icon: 'success',
           title: '登入成功',
           showConfirmButton: false,
           timer: 2000,
-        })
-        router.push('/member/profile')
+        });
+        router.push('/member/profile');
       } else {
         Swal.fire({
           position: 'top',
@@ -75,23 +111,30 @@ export default function LoginPage() {
           title: '登入失敗',
           showConfirmButton: false,
           timer: 2000,
-        })
+        });
       }
     } catch (error) {
-      console.error('Login error:', error)
+      console.error('Login error:', error);
     }
-  }
+  };
 
+  // 處理輸入框變化
   const handleChange = (e) => {
     setData({
       ...data,
       [e.target.name]: e.target.value,
     })
   }
-
+  // 處理輸入框失去焦點
   const handleBlur = (e) => {
     validateFields()
   }
+
+  //處理Remember Me 變化
+  const handleRememberMeChange = () =>{
+    setrememberMe(!rememberMe)
+  }
+
 
   return (
     <section className={style['login-section']}>
@@ -99,8 +142,7 @@ export default function LoginPage() {
         .error {
           color: red;
           font-size: 13px;
-        }
-      `}</style>
+        }`}</style>
       <div className="col-6">
         <div className={style['login-headline']}>
           <h2>會員專區</h2>
@@ -139,8 +181,8 @@ export default function LoginPage() {
                               onBlur={handleBlur}
                               placeholder="請輸入信箱"
                             />
-                            <div className="error">{errors.m_account}</div>
                           </div>
+                          <div className="error">{errors.m_account}</div>
                         </div>
                         <div
                           className={`form-group row ${style['account-box']}`}
@@ -152,24 +194,37 @@ export default function LoginPage() {
                             <FaStarOfLife className={style['icon-padding']} />
                             會員密碼
                           </label>
-                          <div className="col-md-6">
-                            <input
-                              className="form-control"
-                              style={{ borderRadius: '10px' }}
-                              type="password"
-                              id="m_pwd"
-                              name="m_pwd"
-                              value={data.m_pwd}
-                              onChange={handleChange}
-                              onBlur={handleBlur}
-                              placeholder="請輸入4-6位數密碼"
-                            />
-                            <div className="error">{errors.m_pwd}</div>
-                          </div>
+                          <div className="col-md-6" style={{ position: 'relative' }}>
+                          <input
+                            className="form-control"
+                            style={{ borderRadius: '10px' }}
+                            id="m_pwd"
+                            name="m_pwd"
+                            type={showPassword ? 'text' : 'password'}
+                            value={data.m_pwd}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            placeholder="請輸入密碼"
+                          />
+                          <button
+                          type="button"
+                          className={style['show-password']}
+                          onClick={(e) => {
+                            e.preventDefault()
+                            setShowPassword(!showPassword)}}
+                          >
+                            {!showPassword ? <FaEyeSlash /> : <FaEye />}
+                          </button>
+                        </div>
+                          <div className="error">{errors.m_pwd}</div>
                         </div>
                         <div className={`checkbox ${style['check-remember']}`}>
                           <label>
-                            <input type="checkbox" />
+                            <input  
+                              type="checkbox"
+                              checked={rememberMe}
+                              onChange={handleRememberMeChange}
+                            />
                             &nbsp;Remember me
                           </label>
                         </div>
